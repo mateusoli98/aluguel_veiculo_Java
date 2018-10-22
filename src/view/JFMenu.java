ff88760a5c57b48cce90d2ff0af871c297acb357
Amauri de Souza Teixeira <amauri2015gg@gmail.com>
2018-10-22 20:14:50 -0300

    UPDATE - population table veichles and create form

--- src/view/JFMenu.java
+++ src/view/JFMenu.java
@@ -1,6 +1,6 @@
-
 package view;
 
+import static java.lang.System.exit;
 import model.Cliente;
 
 public class JFMenu extends javax.swing.JFrame {
@@ -8,42 +8,94 @@ public class JFMenu extends javax.swing.JFrame {
     public JFMenu(Cliente cliente) {
 
         initComponents();
-       jLabel1.setText(cliente.getNome());
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        menuMain = new javax.swing.JMenuBar();
+        menAlugar = new javax.swing.JMenu();
+        itemVeiculos = new javax.swing.JMenuItem();
+        menuSair = new javax.swing.JMenu();
+        itemSairConta = new javax.swing.JMenuItem();
+        itemSairPrograma = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        menAlugar.setText("Alugar");
+
+        itemVeiculos.setText("Veiculos");
+        itemVeiculos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemVeiculosActionPerformed(evt);
+            }
+        });
+        menAlugar.add(itemVeiculos);
+
+        menuMain.add(menAlugar);
+
+        menuSair.setText("Sair");
+
+        itemSairConta.setText("Conta");
+        itemSairConta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemSairContaActionPerformed(evt);
+            }
+        });
+        menuSair.add(itemSairConta);
+
+        itemSairPrograma.setText("Programa");
+        itemSairPrograma.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemSairProgramaActionPerformed(evt);
+            }
+        });
+        menuSair.add(itemSairPrograma);
+
+        menuMain.add(menuSair);
+
+        setJMenuBar(menuMain);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(76, 76, 76)
-                .addComponent(jLabel1)
-                .addContainerGap(324, Short.MAX_VALUE))
+            .addGap(0, 579, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(69, 69, 69)
-                .addComponent(jLabel1)
-                .addContainerGap(231, Short.MAX_VALUE))
+            .addGap(0, 480, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
- 
+    private void itemSairProgramaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemSairProgramaActionPerformed
+        exit(0);
+    }//GEN-LAST:event_itemSairProgramaActionPerformed
+
+    private void itemSairContaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemSairContaActionPerformed
+        dispose();
+        JFLogin frmLogin = new JFLogin();
+        frmLogin.setVisible(true);
+        frmLogin.setLocationRelativeTo(null);
+    }//GEN-LAST:event_itemSairContaActionPerformed
+
+    private void itemVeiculosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemVeiculosActionPerformed
+       JFVeiculos frmVeiculos =  new JFVeiculos();
+       frmVeiculos.setVisible(true);
+       frmVeiculos.setLocationRelativeTo(null);
+       
+    }//GEN-LAST:event_itemVeiculosActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenuItem itemSairConta;
+    private javax.swing.JMenuItem itemSairPrograma;
+    private javax.swing.JMenuItem itemVeiculos;
+    private javax.swing.JMenu menAlugar;
+    private javax.swing.JMenuBar menuMain;
+    private javax.swing.JMenu menuSair;
     // End of variables declaration//GEN-END:variables
 }
