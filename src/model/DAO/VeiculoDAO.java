c6a2e52189d1b40bd7c0dc175034a0e5aadfffd7
Mateus Oliveira <mateusoli.viana@gmail.com>
2018-11-30 19:44:30 -0200

    UPDATE - ajuste arquitetura

--- src/model/DAO/VeiculoDAO.java
+++ src/model/DAO/VeiculoDAO.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import model.Locacao;
 import model.Veiculo;
 
--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -67,7 +67,5 @@ public class Pessoa {
 
     public void setCelular(int celular) {
         this.celular = celular;
-    }
-    
-    
+    } 
 }
